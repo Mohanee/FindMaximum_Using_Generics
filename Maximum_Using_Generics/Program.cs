71972160da1dc3d1fc028be1af6afaa023309182
Mohanee <mohanee99@gmail.com>
2020-10-21 00:34:58 +0530

    [mohanee] first commit

---
+++ Maximum_Using_Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maximum_Using_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
