8fbf83b1e3575740cac355d6788259b79ac89895
Mohanee <mohanee99@gmail.com>
2020-10-21 21:30:09 +0530

    [mohanee] add method to find maximum for integers

---
+++ Maximum_Using_Generics/FindMaximum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum_Using_Generics
+{
+        public class FindMaximum 
+        {
+            public int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+            {
+                try
+                {
+                    if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                    {
+                        return firstValue;
+                    }
+                    if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                    {
+                        return secondValue;
+                    }
+                    if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+                    {
+                        return thirdValue;
+                    }
+                    else
+                    {
+                        throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return -1;
+                }
+            }
+
+        }
+
+}
+
+
+
--- Maximum_Using_Generics/Program.cs
+++ Maximum_Using_Generics/Program.cs
@@ -7,6 +7,13 @@ namespace Maximum_Using_Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to Find Maximum Program");
+            Console.WriteLine("enter the numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            FindMaximum f = new FindMaximum();
+            Console.WriteLine("maximum of 3 numbers is: " + f.MaximumIntegerNumber(a, b, c));
+
         }
     }
 }
