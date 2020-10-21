239a693cb35805f85e6dbf2c2df1d50ccffca807
Mohanee <mohanee99@gmail.com>
2020-10-22 00:53:08 +0530

    [mohanee]Refactor code to allow more than 3 inputs

--- Maximum_Using_Generics/Program.cs
+++ Maximum_Using_Generics/Program.cs
@@ -11,40 +11,41 @@ namespace Maximum_Using_Generics
             int k = Convert.ToInt32(Console.ReadLine());
             if (k == 1)
             {
-                int a = Convert.ToInt32(Console.ReadLine());
+               /* int a = Convert.ToInt32(Console.ReadLine());
                 int b = Convert.ToInt32(Console.ReadLine());
                 int c = Convert.ToInt32(Console.ReadLine());
-             //   FindMaximum f = new FindMaximum();
-               // Console.WriteLine("maximum of 3 is: "+ f.Maximum<int>(a, b, c));
-
-
-                GenericClass<int> gInt = new GenericClass<int>(a,b,c);
+                //   FindMaximum f = new FindMaximum();
+                // Console.WriteLine("maximum of 3 is: "+ f.Maximum<int>(a, b, c));
+               */
+                int[] arr = { 12, 34, 13, 45, 87, 10 };
+                GenericClass<int> gInt = new GenericClass<int>(arr);
                 gInt.PrintMax();
             }
 
             if (k == 2)
             {
-                float a = float.Parse(Console.ReadLine());
+               /* float a = float.Parse(Console.ReadLine());
                 float b = float.Parse(Console.ReadLine());
                 float c = float.Parse(Console.ReadLine());
                 // FindMaximum f = new FindMaximum();
                 //Console.WriteLine("maximum of 3 is: " + f.Maximum<float>(a, b, c));
-
-                GenericClass<float> gFloat = new GenericClass<float>(a, b, c);
+               */
+                float[] fArr = { 12.0F, 23.0F, 87.98F, 32.67F, 09.89F };
+                GenericClass<float> gFloat = new GenericClass<float>(fArr);
                 gFloat.PrintMax();
 
             }
 
             if (k == 3)
             {
-                string a = Console.ReadLine();
+               /* string a = Console.ReadLine();
                 string b = Console.ReadLine();
                 string c = Console.ReadLine();
                 //FindMaximum f = new FindMaximum();
                 //Console.WriteLine("maximum of 3 is: "+ f.Maximum<string>(a, b, c));
-
-
-                GenericClass<string> gString = new GenericClass<string>(a, b, c);
+               */
+                string[] sArr = { "xenon", "roy", "prudhvi", "mohanee", "rewati" };
+                GenericClass<string> gString = new GenericClass<string>(sArr);
                 gString.PrintMax();
             }
 
